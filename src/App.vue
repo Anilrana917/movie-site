8c16303ba91784348c376b5e59716930b5c0dd7e
ANil RaNa <anilrana917@gmail.com>
2024-07-06 17:19:00 +0530

    title updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header class="app-header">
-      <h1>MoviesFlexx</h1>
+      <h1>MovieAdda</h1>
       <div class="search-box">
         <input
           type="text"
